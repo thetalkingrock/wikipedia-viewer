cd80c6fc30d071cec629c9b9d3ad01fc6c939874
John Rock <jtrock05@gmail.com>
2017-08-18 14:10:43 -0400

    Can now successfully get data from wikipedia api

--- assets/script/wiki.js
+++ assets/script/wiki.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
+	
 	$("#image-container").fadeIn(1500, function () {
 		$("#random-link-container").fadeIn(1500, function () {
 			$("#search-container").fadeIn(1500);
 		});
 	});
+	
+	$(document).keypress(function (e) {
+		if(e.which == 13 && $("input").val() !== "" && $("input").val() != null){
+			
+			var searchTerm = $("input").val();
+			searchTerm = searchTerm.trim();
+			var wikiURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=";
+			wikiURL += searchTerm;
+			wikiURL += "&utf8=&format=json";
+			
+			$.ajax({
+				type: "GET",
+				url: wikiURL,
+				dataType: "jsonp",
+				success: alert("success!")
+			});
+		}
+	});
+	
 });
